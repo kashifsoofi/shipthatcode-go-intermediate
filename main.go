52fbb2fb7091b6682821c7ef614ec1a52c9743a2
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-07-20 19:44:22 +0100

    done

--- main.go
+++ main.go
@@ -6,38 +6,32 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
-	n, _ := strconv.Atoi(sc.Text())
-	sc.Scan()
 	fields := strings.Fields(sc.Text())
-	nums := make([]int, n)
-	for i, f := range fields {
-		nums[i], _ = strconv.Atoi(f)
+	a := make(chan int)
+	b := make(chan int)
+	go func() {
+		defer close(a)
+		for _, f := range fields {
+			n, _ := strconv.Atoi(f)
+			// TODO: send n into channel a
+			a <- n
+		}
+	}()
+	go func() {
+		defer close(b)
+		for n := range a {
+			// TODO: send the square of n into channel b
+			b <- n * n
+		}
+	}()
+	sum := 0
+	for v := range b {
+		sum += v
 	}
-	// split into 4 chunks, goroutine each, sum total
-	var wg sync.WaitGroup
-	var total int
-	var mu sync.Mutex
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		from := i * n / 4
-		to := (i + 1) * n / 4
-		go func(a []int) {
-			defer wg.Done()
-			var sum = 0
-			for _, v := range a {
-				sum += v
-			}
-			defer mu.Unlock()
-			mu.Lock()
-			total += sum
-		}(nums[from:to])
-	}
-	wg.Wait()
-	fmt.Println(total) // replace with the real total
+	fmt.Println(sum)
 }
